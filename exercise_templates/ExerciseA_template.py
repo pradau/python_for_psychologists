8fcac654cd5f80fc05ddce598449b90c9e223ea5
Perry <perry.radau@gmail.com>
2018-10-29 11:02:53 -0600

    adding exercise templates for students

---
+++ exercise_templates/ExerciseA_template.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+#Exercise A
+
+# Assumption 1: indexing
+# It is assumed in the answers below that the descriptions that
+# "row 1" means the first row i.e. 0th index in Python
+# "element (2,2)" means the 2nd row, 2nd column in Python, i.e. indices 1,1.
+# Assumption 2: cumulative effects
+#  The changes in each line of within an Exercise may affect the answers in later parts of the exercise.
+#  e.g. change to A.3b will affect the resulting matrix 'a' of A.3d.
+
+import numpy as np
+
+# 1
+# Your code for 1
+
+
+
+print('#1')
+print('a\n', a)
+print('b\n', b)
+# 2
+# Your code for 2
+
+
+
+print('#2')
+print('c\n', c)
+print('d\n', d)
+print('e\n', e)
+print('f\n', f)
+print('g\n', g)
+
+# 3
+# Your code for 3a)-c)
+
+
+print('#3')
+print('e\n', e)
+print('f\n', f)
+print('e\n', e)
+# Your code for 3d)
+
+
+print('a\n', a)
